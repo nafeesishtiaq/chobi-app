f79cf47789014bd5b516cd48f76f70789a9886e9
nafeesishtiaq <nafeesishtiaq1@gmail.com>
2026-07-02 16:02:36 +0600

    added authprovider

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+import { useAuth } from "./components/auth/AuthProvider";
+import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const { user } = useAuth();
+  const supabase = createClient();
+  const router = useRouter();
+
+  async function handleSignOut() {
+    await supabase.auth.signOut();
+    router.push("/signin");
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      {user ? <p>Signed in as {user.email}</p> : <p>Not signed in.</p>}
+      <button onClick={handleSignOut} className="cursor-pointer">
+        Sign out
+      </button>
+    </div>
   );
 }
